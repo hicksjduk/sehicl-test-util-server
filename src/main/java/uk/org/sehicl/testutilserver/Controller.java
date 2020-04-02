1ae67520f97f0daaabc12abb97a0eeae4faa2c78
Jeremy Hicks <jerhicks@synamedia.com>
2020-04-02 16:21:46 +0100

    More services

--- src/main/java/uk/org/sehicl/testutilserver/Controller.java
+++ src/main/java/uk/org/sehicl/testutilserver/Controller.java
@@ -2,22 +2,30 @@ package uk.org.sehicl.testutilserver;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.Random;
-import java.util.Scanner;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+import java.util.SortedMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonFactoryBuilder;
 import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sendgrid.helpers.mail.Mail;
 
 import redis.embedded.RedisServer;
 import uk.org.sehicl.website.users.User;
@@ -29,13 +37,13 @@ import uk.org.sehicl.website.users.impl.RedisDatastore;
 public class Controller
 {
     @Autowired
-    RedisServer redisServer;
+    private RedisServer redisServer;
 
-    UserDatastore userDatastore;
+    private UserDatastore userDatastore;
 
-    User adminUser;
+    private final SortedMap<LocalDateTime, Mail> mails = new ConcurrentSkipListMap<>();
 
-    @RequestMapping(path = "/recaptcha")
+    @RequestMapping(path = "/recaptcha", produces = "application/json")
     public String approveRecaptcha(HttpServletRequest req) throws IOException
     {
         StringWriter sw = new StringWriter();
@@ -49,72 +57,107 @@ public class Controller
         return answer;
     }
 
-    @RequestMapping(path = "/adminCredentials")
-    public String adminCredentials(HttpServletRequest req) throws IOException
+    @RequestMapping(path = "/userStatus/{email}")
+    public String userCheck(HttpServletRequest req, HttpServletResponse resp,
+            @PathVariable String email) throws IOException
     {
+        User user = userDatastore.getUserByEmail(email);
+        if (user != null)
+            return "" + user.getStatus();
+        resp.setStatus(HttpStatus.NOT_FOUND.value());
+        return "";
+    }
+
+    @RequestMapping(path = "/v3/mail/send", method = RequestMethod.POST, produces = "application/json")
+    public String sendMail(HttpServletRequest req) throws IOException
+    {
+        ObjectMapper mapper = new ObjectMapper();
+        Mail mail = mapper.readValue(req.getInputStream(), Mail.class);
+        mails.put(LocalDateTime.now(), mail);
+        return mapper.writeValueAsString(mail);
+    }
+
+    @RequestMapping(path = "/lastMailTime")
+    public String lastMailTime(HttpServletRequest req) throws IOException
+    {
+        LocalDateTime answer;
+        try
+        {
+            answer = mails.lastKey();
+        }
+        catch (NoSuchElementException ex)
+        {
+            answer = LocalDateTime.of(0, 1, 1, 0, 0);
+        }
+        return answer.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+    }
+
+    @RequestMapping(path = "/mailsSince/{dateTime}", produces = "application/json")
+    public String mailsSince(HttpServletRequest req, @PathVariable String dateTime)
+            throws IOException
+    {
+        LocalDateTime dt = LocalDateTime.parse(dateTime, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        ObjectMapper mapper = new ObjectMapper();
         StringWriter sw = new StringWriter();
         try (JsonGenerator generator = new JsonFactoryBuilder().build().createGenerator(sw))
         {
-            generator.writeStartObject();
-            generator.writeStringField("id", adminUser.getEmail());
-            generator.writeStringField("pw", adminUser.getDecodedPassword());
-            generator.writeEndObject();
+            generator.writeStartArray();
+            for (Entry<LocalDateTime, Mail> e : mails
+                    .tailMap(dt.plus(1, ChronoUnit.MICROS))
+                    .entrySet())
+            {
+                generator.writeStartObject();
+                generator
+                        .writeStringField("dateTime",
+                                e.getKey().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+                generator.writeStringField("mail", mapper.writeValueAsString(e.getValue()));
+                generator.writeEndObject();
+            }
+            generator.writeEndArray();
         }
-        String answer = sw.toString();
-        return answer;
+        return sw.toString();
     }
 
-    @RequestMapping(path = "/v3/mail/send", method = RequestMethod.POST)
-    public String sendMail(HttpServletRequest req) throws IOException
+    @RequestMapping(path = "/clearUsers")
+    public String clearUsers(HttpServletRequest req) throws IOException
     {
-        String body;
-        try (Scanner scanner = new Scanner(req.getInputStream()))
-        {
-            body = scanner.useDelimiter("\\A").next();
-        }
-        System.out.println(body);
+        userDatastore.getAllUserIds().stream().forEach(userDatastore::deleteUser);
         return "";
+    }
+
+    @RequestMapping(path = "/createUser", method = RequestMethod.POST)
+    public String createUser(HttpServletRequest req) throws IOException
+    {
+        User user = userDatastore
+                .createUser(req.getParameter("email"), req.getParameter("name"), null,
+                        req.getParameter("active") != null ? Status.ACTIVE : Status.INACTIVE,
+                        req.getParameter("password"));
+        String role = req.getParameter("role");
+        if (role != null)
+        {
+            user.getRoles().add(role);
+            userDatastore.updateUser(user);
+        }
+        return String.format("%d", user.getId());
     }
 
     @PostConstruct
     public void start()
     {
-        boolean startedNew = true;
         try
         {
             redisServer.start();
         }
         catch (Throwable ex)
         {
-            startedNew = false;
         }
         userDatastore = new RedisDatastore(
                 String.format("redis://%s:%d", "localhost", Application.REDIS_PORT));
-        if (!startedNew)
-        {
-            userDatastore.getAllUserIds().stream().forEach(userDatastore::deleteUser);
-        }
-        adminUser = userDatastore
-                .createUser("admin@sehicl.org.uk", "Admin", "", Status.ACTIVE, randomString(40));
     }
 
     @PreDestroy
     public void stop()
     {
         redisServer.stop();
-    }
-
-    private String randomString(int length)
-    {
-        int[] chars = Stream
-                .of(IntStream.rangeClosed('a', 'z'), IntStream.rangeClosed('A', 'Z'),
-                        IntStream.rangeClosed('0', '9'))
-                .flatMapToInt(ch -> ch)
-                .toArray();
-        StringBuilder sb = new StringBuilder(length);
-        Random rand = new Random();
-        for (int i = 0; i < length; i++)
-            sb.append((char) chars[rand.nextInt(chars.length)]);
-        return sb.toString();
     }
 }
